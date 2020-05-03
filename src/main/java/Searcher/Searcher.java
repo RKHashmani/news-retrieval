f0dfde31966644068fa26f18a60c71cf09ef6d5d
Raheem Hashmani <hashmani.raheem@metu.edu.tr>
2020-05-03 07:23:44 +0300

    Added Searcher function

---
+++ src/main/java/Searcher/Searcher.java
@@ -0,0 +1,45 @@
+package Searcher;
+
+import java.io.File;
+import java.io.IOException;
+
+import Constants.LuceneConstants;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+public class Searcher {
+
+    IndexSearcher indexSearcher;
+    QueryParser queryParser;
+    Query query;
+    StandardAnalyzer standardAnalyzer = new StandardAnalyzer(); //Using the standard Analyzer for now
+
+    public Searcher(String indexDirectoryPath) throws IOException { //Only searches Content for now.
+        Directory indexDirectory = FSDirectory.open(new File(indexDirectoryPath).toPath());
+        IndexReader reader = DirectoryReader.open(indexDirectory);
+        indexSearcher = new IndexSearcher(reader);
+        queryParser = new QueryParser(LuceneConstants.CONTENTS, standardAnalyzer);
+    }
+
+    public TopDocs search( String searchQuery) throws IOException, ParseException {
+        query = queryParser.parse(searchQuery);
+        return indexSearcher.search(query, LuceneConstants.MAX_SEARCH);
+    }
+
+    public Document getDocument(ScoreDoc scoreDoc) throws CorruptIndexException, IOException {
+        return indexSearcher.doc(scoreDoc.doc);
+    }
+
+}
--- src/main/java/Searcher/SearcherMain.java
+++ src/main/java/Searcher/SearcherMain.java
@@ -1,4 +1,44 @@
 package Searcher;
 
+import java.io.IOException;
+
+import Constants.LuceneConstants;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+
 public class SearcherMain {
+
+    static String standard = "src/main/resources/StandardIndex";
+    static String stopwords = "src/main/resources/StopWordsIndex";
+
+    Searcher searcher; // = new Searcher(standard);
+
+    public static void main(String[] args) {
+        SearcherMain tester;
+        try {
+            tester = new SearcherMain();
+            tester.search("2008-01-01");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void search(String searchQuery) throws IOException, ParseException {
+        searcher = new Searcher(stopwords);
+        long startTime = System.currentTimeMillis();
+        TopDocs hits = searcher.search(searchQuery);
+        long endTime = System.currentTimeMillis();
+
+        System.out.println(hits.totalHits +
+                " documents found. Time :" + (endTime - startTime));
+        for(ScoreDoc scoreDoc : hits.scoreDocs) {
+            Document doc = searcher.getDocument(scoreDoc);
+            System.out.println("Article: "
+                    + doc.get(LuceneConstants.HEADER));
+        }
+    }
 }
