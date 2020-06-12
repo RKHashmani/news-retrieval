9226ae85ed2981485c0a515ce45d27ce6e0d1b51
Raheem Hashmani <hashmani.raheem@metu.edu.tr>
2020-06-12 18:58:08 +0300

    Added NDCG Test

--- Evaluation/RecallPrecision.py
+++ Evaluation/RecallPrecision.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from math import sqrt
+import math
 from math import ceil
 import matplotlib
 matplotlib.use('TkAgg')
@@ -9,8 +9,14 @@ import matplotlib.pyplot as plt
 #relevant = [[20,2], [56,3], [57,3], [58,3], [19,4]]
 #retrieved = [[3,0], [20,2], [56,3], [57,3], [58,3], [19,4], [1,0], [300,0]]
 
-relevant = [[1,2], [4,3], [6,3], [9,3], [14,4], [17,0], [19,2], [21,3], [23,3], [29,3], [36,4], [37,0], [38,0]]
-retrieved = [[22,2], [4,3], [36,3], [39,3], [38,4], [6,0], [24,2], [15,3], [23,3], [14,3], [19,4], [18,0], [20,0], [8,0], [1,0]]
+# For testing Recall/Prec plots
+#relevant = [[1,2], [4,3], [6,3], [9,3], [14,4], [17,0], [19,2], [21,3], [23,3], [29,3], [36,4], [37,0], [38,0]]
+#retrieved = [[22,2], [4,3], [36,3], [39,3], [38,4], [6,0], [24,2], [15,3], [23,3], [14,3], [19,4], [18,0], [20,0], [8,0], [1,0]]
+
+# For Testing NDCG:
+relevant = [[1,3], [2,3], [3,3], [4,2], [5,2], [6,1], [7,1], [8,0], [9,0], [10,0]]
+retrieved = [[5,2], [2,3], [1,3], [10,0], [6,1], [4,2], [7,1], [9,0], [3,3], [8,0]]
+
 
 def RetrieveCheck (retrieved, relevant):
 	for i in range(len(retrieved)):
@@ -122,7 +128,7 @@ def InterpolatedValues (retrieved, relevant):
 def PlotInterpol (retrieved, relevant):
 	interpolRecall = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 	interpolPrecision = InterpolatedValues (retrieved, relevant)
-	print (interpolPrecision)
+	#print (interpolPrecision)
 
 	plt.plot (interpolRecall, interpolPrecision, marker = 'o')
 	plt.xlabel ('Recall')
@@ -131,6 +137,39 @@ def PlotInterpol (retrieved, relevant):
 	plt.legend()
 	plt.savefig('Interpolated.png')
 	plt.show()
+
+def DCG (retrieved, relevant):
+	dcg = retrieved[0][1]
+
+	if len(retrieved) < len(relevant):
+		length = len(retrieved)
+	else:
+		length = len(relevant)
+
+	for i in range(length-1):
+		i+=1
+		dcg += retrieved[i][1] / math.log(i+1,2)
+
+	return dcg
+
+def IDCG (retrieved, relevant):
+	idcg = relevant[0][1]
+
+	if len(retrieved) < len(relevant):
+		length = len(retrieved)
+	else:
+		length = len(relevant)
+
+	for i in range(length-1):
+		i+=1
+		idcg += relevant[i][1] / math.log(i+1,2)
+		
+	return idcg
+
+def NDCG (retrieved, relevant):
+	return DCG(retrieved, relevant) / IDCG(retrieved, relevant)
+
+
 
 
 #print(f"Recall: {basicRecall(retrieved, relevant)}")
@@ -143,4 +182,8 @@ def PlotInterpol (retrieved, relevant):
 
 #PlotPrecRecall(retrieved, relevant)
 #InterpolatedValues(retrieved,relevant)
-PlotInterpol(retrieved, relevant)
+#PlotInterpol(retrieved, relevant)
+
+print(f"DCG: {DCG(retrieved, relevant)}")
+print(f"IDCG: {IDCG(retrieved, relevant)}")
+print(f"NDCG: {NDCG(retrieved, relevant)}")
