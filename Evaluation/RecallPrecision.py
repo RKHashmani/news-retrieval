7ae783e77f41817e6bf2acd0959531fbaf426684
Raheem Hashmani <hashmani.raheem@metu.edu.tr>
2020-06-12 23:25:13 +0300

    Added Averaged Interpolated Plots. Pretty much done with evaluation script.

--- Evaluation/RecallPrecision.py
+++ Evaluation/RecallPrecision.py
@@ -5,13 +5,27 @@ from math import ceil
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import numpy as np
 
-relevant = [[20,2], [56,3], [57,3], [58,3], [19,4]]
-retrieved = [[3,0], [20,2], [56,3], [57,3], [58,3], [19,4], [1,0], [300,0]]
+#Add queries here, in this format:
 
-# For testing Recall/Prec plots
-#relevant = [[1,2], [4,3], [6,3], [9,3], [14,4], [17,0], [19,2], [21,3], [23,3], [29,3], [36,4], [37,0], [38,0]]
-#retrieved = [[22,2], [4,3], [36,3], [39,3], [38,4], [6,0], [24,2], [15,3], [23,3], [14,3], [19,4], [18,0], [20,0], [8,0], [1,0]]
+
+#query7 = [[20,2], [56,3], [57,3], [58,3], [19,4]]
+#retrieve7 = [[3,0], [20,2], [56,3], [57,3], [58,3], [19,4], [1,0], [300,0]]
+
+query6 = [[99,2], [115,3], [257,3], [258,3]]
+retrieve6 = [[99,2], [20,2], [115,3], [257,3], [58,3], [19,4], [1,0], [300,0]]
+
+
+
+# For testing Recall/Prec plot. Ignore:
+relevant = [[1,2], [4,3], [6,3], [9,3], [14,4], [17,0], [19,2], [21,3], [23,3], [29,3], [36,4], [37,0], [38,0]]
+retrieved = [[22,2], [4,3], [36,3], [39,3], [38,4], [6,0], [24,2], [15,3], [23,3], [14,3], [19,4], [18,0], [20,0], [8,0], [1,0]]
+
+relevant2 = [[5,2], [15,3], [25,3], [36,3]]
+retrieved2 = [[25,2], [12,3], [4,3], [5,3], [13,4], [16,0], [35,2], [32,3], [28,3], [17,3], [21,4], [15,0], [23,0], [36,0], [1,0]]
+
+
 
 # For Testing NDCG:
 #relevant = [[1,3], [2,3], [3,3], [4,2], [5,2], [6,1], [7,1], [8,0], [9,0], [10,0]]
@@ -169,8 +183,35 @@ def IDCG (retrieved, relevant):
 def NDCG (retrieved, relevant):
 	return DCG(retrieved, relevant) / IDCG(retrieved, relevant)
 
+def interpolated (retrieve1, query1, retrieve2, query2, retrieve3, query3): #Add more query variables here
+
+	interpolRecall = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+
+	TotalQueries = 3 #Set the number of total queries present in the argument.
+
+	# Add another line for each query
+	interpol_1 = InterpolatedValues(retrieve1, query1)
+	interpol_2 = InterpolatedValues(retrieve2, query2)
+	interpol_3 = InterpolatedValues(retrieve3, query3)
+
+	numerator = [interpol_1[i] + interpol_2[i] + interpol_3[i] for i in range(len(interpol_1))] #Add a new interpol_X[i]
+	averaged = [numerator[i] / TotalQueries for i in range(len(interpol_1))]
+	
+	plt.plot (interpolRecall, averaged, marker = 'o')
+	plt.xlabel ('Recall')
+	plt.ylabel ('Precision')
+	plt.title ('Averaged 11-point Precision/Recall Graph for Standard Analyzer')
+	plt.grid()
+	plt.legend()
+	plt.savefig('AveragedInterpolated.png')
+	plt.show()
+
+	return averaged
 
 
+
+
+## Don't Delete, this is for potential probelm debugging. 
 
 #print(f"Recall: {basicRecall(retrieved, relevant)}")
 #print(f"Precision: {basicPrecision(retrieved, relevant)}")
@@ -181,9 +222,32 @@ def NDCG (retrieved, relevant):
 #print(len(PrecTable(retrieved,relevant)))
 
 #PlotPrecRecall(retrieved, relevant)
-#print(f"Interpolated Precision: {InterpolatedValues(retrieved, relevant)}")
+#print(f"Interpolated Precision: {InterpolatedValues(retrieved2, relevant2)}")
 #PlotInterpol(retrieved, relevant)
 
-print(f"DCG: {DCG(retrieved, relevant)}")
-print(f"IDCG: {IDCG(retrieved, relevant)}")
+#print(f"DCG: {DCG(retrieved, relevant)}")
+#print(f"IDCG: {IDCG(retrieved, relevant)}")
+#print(f"NDCG: {NDCG(retrieved, relevant)}")
+
+## OUTPUT
+
+#For Query1
+print ('For Query1:')
+
 print(f"NDCG: {NDCG(retrieved, relevant)}")
+print(f"Recall: {basicRecall(retrieved, relevant)}")
+print(f"Precision: {basicPrecision(retrieved, relevant)}")
+print(f"F1: {F1(retrieved, relevant)}")
+print('\n')
+
+#For Query2
+print ('For Query2:')
+
+print(f"NDCG: {NDCG(retrieved2, relevant2)}")
+print(f"Recall: {basicRecall(retrieved2, relevant2)}")
+print(f"Precision: {basicPrecision(retrieved2, relevant2)}")
+print(f"F1: {F1(retrieved2, relevant2)}")
+print('\n')
+
+
+interpolated(retrieved, relevant, retrieved2, relevant2, retrieve6, query6)
