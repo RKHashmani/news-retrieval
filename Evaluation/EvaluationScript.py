4ebf4f98cb04bfc2f52618ec7f8ed1d51e79a24b
Raheem Hashmani <hashmani.raheem@metu.edu.tr>
2020-06-19 14:39:11 +0300

    Added a slightly modified Evaluation that takes into account the weird -1 relation levels, but in their correct order.

--- Evaluation/EvaluationScript.py
+++ Evaluation/EvaluationScript.py
@@ -402,6 +402,197 @@ def EVALUATE (retrieve1, query1):
 
 #OUTPUT
 
+## OUTPUT
+
+#FOR STANDARD ANALYZER
+
+print('***** STANDARD: ******')
+
+#For Query1
+print ('For Query1:')
+EVALUATE(retrieve1, query1)
+print('\n')
+
+#For Query2
+print ('For Query2:')
+EVALUATE(retrieve2, query2)
+print('\n')
+
+#For Query3
+print ('For Query8:')
+EVALUATE(retrieve8, query8)
+print('\n')
+
+#For Query4
+print ('For Query10:')
+EVALUATE(retrieve10, query10)
+print('\n')
+
+#For Query5
+print ('For Query23:')
+EVALUATE(retrieve23, query23)
+print('\n')
+
+#For Query6
+print ('For Query29:')
+EVALUATE(retrieve29, query29)
+print('\n')
+
+#For Query7
+print ('For Query39:')
+EVALUATE(retrieve39, query39)
+print('\n')
+
+#For Query8
+print ('For Query40:')
+EVALUATE(retrieve40, query40)
+print('\n')
+
+#For Query9
+print ('For Query51:')
+EVALUATE(retrieve51, query51)
+print('\n')
+
+#For Query10
+print ('For Query53:')
+EVALUATE(retrieve53, query53)
+print('\n')
+
+#For Query11
+print ('For Query157:')
+EVALUATE(retrieve157, query157)
+print('\n')
+
+#For Query12
+print ('For Query225:')
+EVALUATE(retrieve225, query225)
+print('\n')
+
+#FOR NO STOPWORD ANALYZER
+print('***** NO STOPWORD: ******')
+
+#For Query1
+print ('For Query1:')
+EVALUATE(retrieve1STOP, query1)
+print('\n')
+
+#For Query2
+print ('For Query2:')
+EVALUATE(retrieve2STOP, query2)
+print('\n')
+
+#For Query3
+print ('For Query8:')
+EVALUATE(retrieve8STOP, query8)
+print('\n')
+
+#For Query4
+print ('For Query10:')
+EVALUATE(retrieve10STOP, query10)
+print('\n')
+
+#For Query5
+print ('For Query23:')
+EVALUATE(retrieve23STOP, query23)
+print('\n')
+
+#For Query6
+print ('For Query29:')
+EVALUATE(retrieve29STOP, query29)
+print('\n')
+
+#For Query7
+print ('For Query39:')
+EVALUATE(retrieve39STOP, query39)
+print('\n')
+
+#For Query8
+print ('For Query40:')
+EVALUATE(retrieve40STOP, query40)
+print('\n')
+
+#For Query9
+print ('For Query51:')
+EVALUATE(retrieve51STOP, query51)
+print('\n')
+
+#For Query10
+print ('For Query53:')
+EVALUATE(retrieve53STOP, query53)
+print('\n')
+
+#For Query11
+print ('For Query157:')
+EVALUATE(retrieve157STOP, query157)
+print('\n')
+
+#For Query12
+print ('For Query225:')
+EVALUATE(retrieve225STOP, query225)
+print('\n')
+
+#FOR WHITESPACE ANALYZER
+print('***** WHITESPACE: ******')
+
+#For Query1
+print ('For Query1:')
+EVALUATE(retrieve1WHITE, query1)
+print('\n')
+
+#For Query2
+print ('For Query2:')
+EVALUATE(retrieve2WHITE, query2)
+print('\n')
+
+#For Query3
+print ('For Query8:')
+EVALUATE(retrieve8WHITE, query8)
+print('\n')
+
+#For Query4
+print ('For Query10:')
+EVALUATE(retrieve10WHITE, query10)
+print('\n')
+
+#For Query5
+print ('For Query23:')
+EVALUATE(retrieve23WHITE, query23)
+print('\n')
+
+#For Query6
+print ('For Query29:')
+EVALUATE(retrieve29WHITE, query29)
+print('\n')
+
+#For Query7
+print ('For Query39:')
+EVALUATE(retrieve39WHITE, query39)
+print('\n')
+
+#For Query8
+print ('For Query40:')
+EVALUATE(retrieve40WHITE, query40)
+print('\n')
+
+#For Query9
+print ('For Query51:')
+EVALUATE(retrieve51WHITE, query51)
+print('\n')
+
+#For Query10
+print ('For Query53:')
+EVALUATE(retrieve53WHITE, query53)
+print('\n')
+
+#For Query11
+print ('For Query157:')
+EVALUATE(retrieve157WHITE, query157)
+print('\n')
+
+#For Query12
+print ('For Query225:')
+EVALUATE(retrieve225WHITE, query225)
+print('\n')
 
 #For All Queries:
 print("For Standard Analyzer")
